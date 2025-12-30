936083bebee5e10d48c6681dba25dd7dadd6c916
Sundaram <thisnewcomputerismine2020@gmail.com>
2025-12-31 02:34:31 +0530

    fix :cors error on server

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:5173'||'https:/spurbot.samosa.wtf',
+  origin: [ 'http://localhost:5173' ,'https://spurbot.samosa.wtf',],
   credentials: true,
 }));
 app.use(express.json({ limit: '10mb' }));
